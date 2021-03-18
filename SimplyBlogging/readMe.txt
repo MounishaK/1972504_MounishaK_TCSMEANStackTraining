a15c537632f6324851849d6b4fb10b86e03cb6b8
MounishaK <mounisha098@gmail.com>
2021-03-17 21:24:58 -0500

    Simply Blogging Assesment

---
+++ SimplyBlogging/readMe.txt
@@ -0,0 +1,10 @@
+"simply Blogging" is a phase 1 end assesment of Mean Stack Training.
+
+Steps:
+	 
+	- Created a html file named simplyBlog.html.
+	- And then, linked a bootstrap url in the simplyBlog.html to get the nice effects in the html page.
+	- Linked an external css file named simplyBlog.css to simplyBlog.html.
+	- Also, linked .js file named simplyBlog.js to simplyBlog.html for the "Add Blog" functionality.
+	- lastly, took an output screenshot for reference.
+
--- SimplyBlogging/source code/simplyBlog.js
+++ SimplyBlogging/source code/simplyBlog.js
@@ -13,8 +13,6 @@ function retrive()
 
 function onFormSubmit()
 {
-    alert("Data added...");
-
     var title = document.getElementById("title").value;
     var articles = document.getElementById("articles").value;
     var imageInfo = document.getElementById("image").files[0].name;
@@ -22,8 +20,16 @@ function onFormSubmit()
     console.log(articles);
     console.log(imageInfo);
     document.getElementById("titleInfo").innerHTML=title;
-    document.getElementById("descInfo").innerHTML=articles;
+    document.getElementById("articleInfo").innerHTML=articles;
     document.getElementById("imageInfo").src=imageInfo;
+   
+    var imageInfo1 = document.getElementById("image").files[0].name;
+    console.log(title)
+    console.log(articles);
+    console.log(imageInfo1);
+    document.getElementById("titleInfo1").innerHTML=title;
+    document.getElementById("articleInfo1").innerHTML=articles;
+    document.getElementById("imageInfo1").src=imageInfo1;
     resetData();
     storeInSession();
 }
@@ -33,65 +39,4 @@ function resetData()
     document.getElementById("title").value ="";
     document.getElementById("articles").value = "";
     document.getElementById("image").value = "";
-        
 }
-
-/*   var data = readFormData();
-    //insertNewRecord(data);
-    resetData();
-    budgetObj.push(data); 
-    storeInSession();
-  
-
-
-function readFormData()
-{
-    var obj = {}; // empty object
-    obj.clientname = document.getElementById("clientname").innerHTML = clientname ;
-    obj.projectname = document.getElementById("projectname").innerHTML = projectname ;
-    obj.projectbudget = document.getElementById("projectbudget").src = projectbudget;
-    console.log(obj);
-    return obj;
-}
-
-function insertNewRecord(data)
-{   //var budgetObj = {};
-    var table = document.getElementById("AnnualBudget")
-    var body = table.getElementsByTagName("tbody")[0];
-    for(var i =0; i<data.length;i++)
-    {
-        var newRow = body.insertRow(body.length); //row created
-
-        var cell1 = newRow.insertCell(0); //cell created
-         cell1.innerHTML = data[i].clientname; // value passed
-
-        var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data[i].projectname;
-
-        var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data[i].projectbudget+"<br/>";
-    }
-}
-
-function resetData()
-{
-    document.getElementById("clientname").value ="";
-    document.getElementById("projectname").value = "";
-    document.getElementById("projectbudget").value = "";
-        
-}
-*/
-
-/*
-function dispayData()
-{
-    var obj1 = sessionStorage.getItem("clientname");
-    document.getElementById("clientname").innerHTML = obj1;
-
-    var obj2 = sessionStorage.getItem("projectname");
-    document.getElementById("projectname").innerHTML = obj2;
-
-    var obj3 = sessionStorage.getItem("projectbudget");
-    document.getElementById("projectbudget").innerHTML = obj3;
-
-}*/
