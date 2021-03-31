af9130a371cb9755c3c3d934041016d2af85d95c
MounishaK <mounisha098@gmail.com>
2021-03-30 23:19:25 -0500

    Online Test Application Assessment

--- OnlineTestApplication/online-test-application/src/app/testquestions.model.ts
+++ OnlineTestApplication/online-test-application/src/app/testquestions.model.ts
@@ -16,20 +16,3 @@ export class TestQuestions{ // this class is use to map json data.
     }
 }
 
-/*
-export class Option {
-    id: number;
-    questionId: number;
-    name: string;
-    isAnswer: boolean;
-    selected: boolean;
-
-    constructor(data: any) {
-        data = data || {};
-        this.id = data.id;
-        this.questionId = data.questionId;
-        this.name = data.name;
-        this.isAnswer = data.isAnswer;
-    }
-}
-*/
--- OnlineTestApplication/online-test-application/src/app/testquestions.service.ts
+++ OnlineTestApplication/online-test-application/src/app/testquestions.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import { TestQuestions } from './testquestions.model';
-//import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class TestquestionsService {
 
   constructor(public http:HttpClient) { }
-/*
-  loadTestQuestions():Observable<TestQuestions[]>
-  {
-    return this.http.get<TestQuestions[]>("/assets/testquestions.json");
-  }
 
-*/
 
   get(url: string) {
     return this.http.get(url);
